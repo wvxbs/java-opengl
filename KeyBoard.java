96b13bfe9ee22e253bf4b44ca9348680cde3cccb
wvxbs <gabriel.ferreira7854@gmail.com>
2023-10-16 09:33:01 -0300

    app start

---
+++ KeyBoard.java
@@ -0,0 +1,33 @@
+package aula04.exemplo01;
+import com.jogamp.newt.event.KeyEvent;
+import com.jogamp.newt.event.KeyListener;
+/**
+ *
+ * @author Kakugawa
+ */
+public class KeyBoard implements KeyListener{
+    private Cena cena;
+    
+    public KeyBoard(Cena cena){
+        this.cena = cena;
+    }
+    
+    @Override
+    public void keyPressed(KeyEvent e) {        
+        System.out.println("Key pressed: " + e.getKeyCode());
+        if(e.getKeyCode() == KeyEvent.VK_ESCAPE)
+            System.exit(0);
+        
+        if(e.getKeyChar() == 'a')
+            System.out.println("Pressionou tecla a");
+
+        if(e.getKeyCode() == KeyEvent.VK_UP) {
+            System.out.println("Pressionou tecla up");
+            cena.reshape(cena.Resize());
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) { }
+
+}
